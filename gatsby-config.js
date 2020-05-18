9c33ce3018ebadb06a7f71eb9a92cd5253d0c538
kvzivn <hello@kevinivan.com>
2020-05-18 17:45:03 +0800

    add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,8 +71,23 @@ module.exports = {
         respectDNT: true,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5FFS28T",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
+  ]
 }
